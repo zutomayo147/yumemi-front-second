cb77cba4f01f54e6a9804b41ff6ceb1f66969073
hedgehog <42014197+zutomayo147@users.noreply.github.com>
2023-04-26 15:45:31 +0900

    refactor: 動的インポートを用いた遅延ロード

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
+import dynamic from 'next/dynamic';
+
 import Header from '@/components/layouts/Header';
 import PrefList from '@/features/prefList/PrefList';
-import PopulationChart from '@/features/showGraph/PopulationChart';
+const PopulationChart = dynamic(
+  () => import('@/features/showGraph/PopulationChart'),
+  {
+    ssr: false, // サーバーサイドレンダリングを無効にする
+  },
+);
 
 export default function Home() {
   return (
